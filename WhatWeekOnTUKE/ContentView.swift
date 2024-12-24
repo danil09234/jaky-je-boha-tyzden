ccea07ff1146a484d7285be37d0c90765c20ba14
danil09234 <dailzag05@gmail.com>
2024-12-25 00:37:37 +0200

    Initial Commit

---
+++ WhatWeekOnTUKE/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WhatWeekOnTUKE
+//
+//  Created by macbook on 25.12.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WhatWeekOnTUKE/WhatWeekOnTUKEApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatWeekOnTUKEApp.swift
+//  WhatWeekOnTUKE
+//
+//  Created by macbook on 25.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct WhatWeekOnTUKEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
