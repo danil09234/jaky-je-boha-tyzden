05b64f4b1828b3109e0ef2b6ab54c54bd5c7c2e8
danil09234 <dailzag05@gmail.com>
2025-02-14 19:26:25 +0100

    Update time zone in time formatter

--- AppCore/Sources/AppCore/Utils.swift
+++ AppCore/Sources/AppCore/Utils.swift
@@ -4,6 +4,7 @@ import Foundation
 public func isoStringToDate(_ isoString: String) -> Date {
     let formatter = ISO8601DateFormatter()
     formatter.formatOptions = [.withFullDate]
+    formatter.timeZone = TimeZone(secondsFromGMT: 3600)!
     guard let date = formatter.date(from: isoString) else {
         fatalError("Invalid date string: \(isoString)")
     }
@@ -13,7 +14,7 @@ public func isoStringToDate(_ isoString: String) -> Date {
 public func createDateFormatter() -> DateFormatter {
     let formatter = DateFormatter()
     formatter.locale = Locale(identifier: "sk")
-    formatter.timeZone = TimeZone(secondsFromGMT: 0)
+    formatter.timeZone = TimeZone(secondsFromGMT: 3600)!
     formatter.dateFormat = "dd.MM.yyyy"
     return formatter
 }
